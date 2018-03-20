3c349295b793dab5fb8ac33fcf3c0e99f8ac1082
Thiago Silveira <thiagovars@gmail.com>
2018-03-19 23:07:47 -0300

    Configurando a base de dados umbler
    Adicionando configuração nas constantes do sistema.

--- admin/application/config/config.php
+++ admin/application/config/config.php
@@ -5,10 +5,10 @@ $config['base_url'] = 'http://localhost/thiagoymariana/admin'; // Base URL inclu
 $config['default_controller'] = 'main'; // Default controller to load
 $config['error_controller'] = 'error'; // Controller used for errors (e.g. 404, 500 etc)
 
-$config['db_host'] = 'localhost'; // Database host (e.g. localhost)
+$config['db_host'] = 'mysql472.umbler.com'; // Database host (e.g. localhost)
 $config['db_name'] = 'tym_db'; // Database name
-$config['db_username'] = 'root'; // Database username
-$config['db_password'] = ''; // Database password
+$config['db_username'] = 'thiagovars'; // Database username
+$config['db_password'] = 'U.EL7|y.o9|UA'; // Database password
 
 define('DB_HOST', 'mysql472.umbler.com');
 define('DB_NAME', 'tym_db');
