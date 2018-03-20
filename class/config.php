e5b2afddfe98dffb00e055a032722e6c8cca9cb0
Thiago Silveira <thiagovars@gmail.com>
2018-03-19 23:13:34 -0300

    Criando conexão com a base de prod
    Colocando a base de dados do front end

--- class/config.php
+++ class/config.php
@@ -1,6 +1,6 @@
 <?php
 
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "");
+define("DB_HOST", "mysql472.umbler.com");
+define("DB_USER", "thiagovars");
+define("DB_PASS", "U.EL7|y.o9|UA");
 define("DB_NAME", "tym_db");
