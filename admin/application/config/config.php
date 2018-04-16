6ccdd82e90e143445dccee4ebd32a06fb3732f58
Thiago Silveira <thiagovars@gmail.com>
2018-04-15 21:57:27 -0300

    Ajustes de conexão com produção

--- admin/application/config/config.php
+++ admin/application/config/config.php
@@ -1,7 +1,19 @@
 <?php 
 include_once(dirname(dirname(dirname(dirname(__FILE__)))).'\inc\ambiente.php');
 switch (ambiente) {
-	case 'PRODUCAO':
+	case 'DESENVOLVIMENTO':
+		$config['base_url'] = 'http://localhost/thiagoymariana/admin'; // Base URL including trailing slash (e.g. http://localhost/)
+		$config['db_name'] = 'tym_db'; // Database name
+		$config['db_username'] = 'root'; // Database username
+		$config['db_password'] = ''; // Database password
+
+		define('DB_HOST', 'localhost');
+		define('DB_NAME', 'tym_db');
+		define('DB_USER', 'root');
+		define('DB_PASS', '');
+		break;
+	
+	default:
 		$config['base_url'] = 'http://thiagoymariana.com/admin'; // Base URL including trailing slash (e.g. http://localhost/)
 		$config['db_host'] = 'mysql472.umbler.com'; // Database host (e.g. localhost)
 		$config['db_name'] = 'tym_db'; // Database name
@@ -12,18 +24,6 @@ switch (ambiente) {
 		define('DB_NAME', 'tym_db');
 		define('DB_USER', 'thiagovars');
 		define('DB_PASS', 'U.EL7|y.o9|UA');
-		break;
-	
-	default:
-		$config['base_url'] = 'http://localhost/thiagoymariana/admin'; // Base URL including trailing slash (e.g. http://localhost/)
-		$config['db_name'] = 'tym_db'; // Database name
-		$config['db_username'] = 'root'; // Database username
-		$config['db_password'] = ''; // Database password
-
-		define('DB_HOST', 'localhost');
-		define('DB_NAME', 'tym_db');
-		define('DB_USER', 'root');
-		define('DB_PASS', '');
 		break;
 }
 
